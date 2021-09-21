eb730ac86ea5984e2e30ac501d65124a7c24a25c
Derus Mantilla <derus@grydd.com>
2021-09-21 01:18:10 -0500

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAwND0Lsx4VXZ0cabWdjb2PT2xDA_FT51E",
+    authDomain: "ventas-simple.firebaseapp.com",
+    databaseURL: "https://ventas-simple.firebaseio.com",
+    projectId: "ventas-simple",
+    storageBucket: "ventas-simple.appspot.com",
+    messagingSenderId: "791435205328",
+    appId: "1:791435205328:web:72e507428005ff16ea1cd5",
+    measurementId: "..."
+  }
 };
 
 /*
